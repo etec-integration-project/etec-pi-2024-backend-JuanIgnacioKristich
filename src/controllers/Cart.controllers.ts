12eae6ef4331a87424631197fba21caa4506fec9
JuanIgnacioKristich <j.kristich@alumno.etec.um.edu.ar>
2023-11-28 23:46:24 -0300

    previa a entrega

--- src/controllers/Cart.controllers.ts
+++ src/controllers/Cart.controllers.ts
@@ -5,11 +5,21 @@ import { AppDataSource } from '../db';
 import Cart from "../entities/Cart" 
 
 
+
+let connection: Connection | null = null;
+
+export const getDatabaseConnection = async (): Promise<Connection> => {
+  if (!connection || !connection.isConnected) {
+    connection = await createConnection();
+  }
+  return connection;
+};
+
 export const registerCart = async (req: Request, res: Response) => {
     const { jsonifiedCart } = req.body;
 
     try {
-
+        const connection = await getDatabaseConnection();
         const cartEntity = new Cart(jsonifiedCart);
 
         await AppDataSource.manager.save(Cart, cartEntity);
